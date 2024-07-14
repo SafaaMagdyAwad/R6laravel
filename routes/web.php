538f5f8b3081b11c66bfd1eec315c71f88a4b2e0
Safaa Magdy <engsafaamagdy1024@gmail.com>
2024-07-14 22:22:48 +0300

    initial commet

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Http\Controllers\ExambleController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::prefix('accounts')->group(
+    function(){
+        Route::get('',function(){
+            return "accounts ";
+        });
+        Route::get('/admin',function(){
+            return "accounts admin ";
+        });
+        Route::get('/user',function(){
+            return "accounts user ";
+        });
+
+    }
+);
+Route::prefix('cars')->group(
+    function(){
+       
+        Route::prefix('usa')->group(
+            function(){
+                Route::get('/ford',function(){
+                    return "cars usa ford ";
+                });
+                Route::get('/tesla',function(){
+                    return "cars usa tesla ";
+                });
+        
+            }
+        );
+
+        Route::prefix('ger')->group(
+            function(){
+                Route::get('/mercedes',function(){
+                    return "cars ger mercedes ";
+                });
+                Route::get('/audi',function(){
+                    return "cars ger audi ";
+                });
+                Route::get('/volkswagen',function(){
+                    return "cars ger volkswagen ";
+                });
+        
+            }
+        );
+
+    }
+);
+
+
+Route::get('login',[ExambleController::class,'login']);
+Route::post('loginPost',[ExambleController::class,'loginPost'])->name('loginPost');
+
